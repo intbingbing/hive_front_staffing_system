d794a016d5936c54633cbfa61394c51c85284a7d
intbingbing <1157962465@qq.com>
2018-01-12 18:39:55 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Container from '@/components/Container'
+import md5 from 'crypto-js/md5'
+import hex from 'crypto-js/enc-hex'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -14,3 +16,17 @@ export default new Router({
   ],
   mode:'history'
 })
+
+router.beforeEach((to, from, next) => {
+    let password='a';
+    console.log(password);
+    let tmp = md5(password);
+    console.log("【0密】："+tmp);
+    let secret = md5(password).toString(hex);
+    console.log('【1密】：'+secret);
+    secret = md5(secret).toString(hex);
+    console.log('【2密】：'+secret);
+    next();
+});
+
+export default router;
